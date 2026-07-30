0c9d4d3ea0bc10b7f3ea8756672659ff3ac84c9d
poojagosika <pooja.gosika@gmail.com>
2026-07-30 10:27:33 +0530

    Added duplicate route to thumbnails router

--- server/src/routes/thumbnails.js
+++ server/src/routes/thumbnails.js
@@ -10,6 +10,7 @@ const {
   bulkDelete,
   bulkTag,
   reorder,
+  duplicateThumbnail,
 } = require("../controllers/thumbnailController");
 
 const router = express.Router();
@@ -22,6 +23,7 @@ router.post("/bulk-tag", bulkTag);
 router.post("/reorder", reorder);
 router.get("/", getThumbnails);
 router.get("/:id", getThumbnail);
+router.post("/:id/duplicate", duplicateThumbnail);
 router.patch("/:id", updateThumbnail);
 router.delete("/:id", deleteThumbnail);
 
